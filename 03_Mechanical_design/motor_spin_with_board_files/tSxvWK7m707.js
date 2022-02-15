0589fb884219f1e1b3e392247aade1bfb25b527c
bluepalace8 <bluepalace8@gmail.com>
2022-02-15 14:55:52 -0500

    chapter 3

---
+++ 03_Mechanical_design/motor_spin_with_board_files/tSxvWK7m707.js
@@ -0,0 +1,5 @@
+if (self.CavalryLogger) { CavalryLogger.start_js_script(document.currentScript); }
+
+__d("QpActionFalcoEvent",["FalcoLoggerInternal","getFalcoLogPolicy_DO_NOT_USE"],(function(a,b,c,d,e,f){"use strict";a=b("getFalcoLogPolicy_DO_NOT_USE")("1744247");c=b("FalcoLoggerInternal").create("qp_action",a);e.exports=c}),null);
+__d("QpImpressionFalcoEvent",["FalcoLoggerInternal","getFalcoLogPolicy_DO_NOT_USE"],(function(a,b,c,d,e,f){"use strict";a=b("getFalcoLogPolicy_DO_NOT_USE")("1744249");c=b("FalcoLoggerInternal").create("qp_impression",a);e.exports=c}),null);
+__d("HelpCenterLocaleTypeaheadDataSourceQuery_facebookRelayOperation",[],(function(a,b,c,d,e,f){e.exports="5021798377831510"}),null);
